3d429237076aae53a1778f6e18e77393805464e2
Jerome Hardaway <jerome@vetswhocode.io>
2017-12-06 13:07:23 -0600

    Managing State of Component

---
+++ src/components/ZipForm.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+
+class ZipForm extends Component {
+  constructor(props) {
+  super(props);
+
+  this.state = {
+    zipcode: ''
+  };
+  this.inputUpdated = this.inputUpdated.bind(this);
+}
+
+  inputUpdated(e) {
+    const { value } e.target;
+
+    this.setState({ zipcode: value });
+  }
+
+  render() {
+    return (
+      <div className="zip-form">
+        <form>
+          <label htmlFor="zipcode">Zip Code</label>
+          <input
+            className="form-control"
+            type="input"
+            name="zipcode"
+            value={this.state.zipcode}
+            onInput={this.inputUpdated}/>
+          <button type="submit" className='btn btn-success'>Check Forecast</button>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default ZipForm;
